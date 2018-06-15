c4e20c879ce1a64daa18dca5dc73ff6b84ba10bc
Aman Kapur <akapur07@gmail.com>
2018-06-15 19:39:39 +0530

    Angular firebase chat room without style

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBSxRe5Bu6BfennOqg7bOvGFOn9bwODEMw",
+    authDomain: "msgoom-db.firebaseapp.com",
+    databaseURL: "https://msgoom-db.firebaseio.com",
+    projectId: "msgoom-db",
+    storageBucket: "msgoom-db.appspot.com",
+    messagingSenderId: "494106204200"
+  }
 };
